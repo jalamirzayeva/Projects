67bab74d8d191dfd2e048ee267126f036c0a09da
Jala Mirzayeva <mirzayevajala3@gmail.com>
2022-03-05 01:32:36 +0400

    consens az layihesi add olundu

--- resources/js/main.js
+++ resources/js/main.js
@@ -15,10 +15,44 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 
 // about slider
 $('.aboutSlider').slick({
-  infinite: true ,
+  infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: true,
   autoplay: true,
   autoplaySpeed: 1000,
 });
+
+$(document).ready(function() {
+  $(".promotions-carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 639,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+// navbar dropdown
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function (e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+// }
